f4d898440942485278c0106c7f87c6346ab2a1b1
nguyenthanhlinh <thanhlinh12314@gmail.com>
2018-04-05 16:56:36 +0700

    implement mothod of arraylist

--- MyList.java
+++ MyList.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 public class MyList<E> implements MyListInterface{
     private int size = 0;
     private static final int DEFAULT_CAPACITY = 10;
@@ -18,12 +20,17 @@ public class MyList<E> implements MyListInterface{
 
     @Override
     public Object remove(int index) {
-        return null;
+        for (int i = index; i < size - 1; i++){
+            elements[i] = elements[i + 1];
+        }
+        size--;
+        return (E) elements[index];
     }
 
     @Override
     public int size() {
-        return 0;
+        size = elements.length;
+        return size;
     }
 
     @Override
@@ -33,33 +40,55 @@ public class MyList<E> implements MyListInterface{
 
     @Override
     public boolean contains(Object o) {
-        return false;
+        boolean isContains = false;
+        for (int i = 0; i < size; i++){
+            if (elements[i] == o){
+                isContains = true;
+                break;
+            }
+        }
+        return isContains;
     }
 
     @Override
     public int indexOf(Object o) {
-        return 0;
+        int index = -1;
+        for (int i = 0; i < size; i++){
+            if (elements[i] == o){
+                index = i;
+            }
+        }
+        return index;
     }
 
     @Override
     public boolean add(Object o) {
-        return false;
+        if (size == elements.length){
+            ensureCapacity(1);
+        }
+        elements[++size] = o;
+        return true;
     }
 
     @Override
     public void ensureCapacity(int minCapacity) {
-        if (size == DEFAULT_CAPACITY){
-
-        }
+        int newSize = elements.length + minCapacity;
+        elements = Arrays.copyOf(elements, newSize);
     }
 
     @Override
     public Object get(int i) {
-        return null;
+        if (i >= size || i < 0){
+            throw new IndexOutOfBoundsException("index: " + i + " size: " + size);
+        }
+        return (E) elements[i];
     }
 
     @Override
     public void clear() {
-
+        for (int i = 0; i < size; i++){
+            elements[i] = null;
+        }
+        size = 0;
     }
 }
